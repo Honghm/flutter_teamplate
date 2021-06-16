01f1dc65093ab24de091c6af55181ebf58f04433
MinhHong <minhhong0001@gmail.com>
2021-06-16 21:54:12 +0700

    add camera

---
+++ lib/camera/constants/custom_pick_method.dart
@@ -0,0 +1,15 @@
+
+part of flutter_template;
+class CustomPickMethod {
+  const CustomPickMethod({
+    required this.icon,
+    required this.name,
+    required this.description,
+    required this.method,
+  });
+
+  final String icon;
+  final String name;
+  final String description;
+  final void Function(BuildContext context) method;
+}
--- lib/flutter_template.dart
+++ lib/flutter_template.dart
@@ -9,7 +9,9 @@ import 'package:flutter/services.dart';
 import 'package:flutter_template/pin_code/cursor_painter.dart';
 import 'package:flutter_template/pin_code/models/platform.dart';
 import 'package:flutter_template/button/bell/circle.dart';
-
+import 'package:flutter/widgets.dart';
+import 'package:wechat_assets_picker/wechat_assets_picker.dart';
+import 'package:wechat_camera_picker/wechat_camera_picker.dart';
 
 part 'button/build_button.dart';
 part 'button/progress_button/progress_button_animation.dart';
@@ -20,3 +22,6 @@ part 'pin_code/models/pin_theme.dart';
 part 'pin_code/pin_code_fields.dart';
 part 'slide/range_slider.dart';
 part 'button/bell/blinking_point.dart';
+part 'camera/constants/custom_pick_method.dart';
+part 'camera/constants/extensions.dart';
+part 'camera/constants/picker_method.dart';
